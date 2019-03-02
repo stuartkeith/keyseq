ee79c439530a90a566211855a2dea1374b684f48
Stuart Keith <stuart@stuartkeith.com>
2019-03-02 16:34:52 +1100

    fix duplicate class name in LabelA

--- src/components/LabelA.js
+++ src/components/LabelA.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function LabelA({ disabled, children }) {
   const className = `
-    dark-gray dib f6 pa2 box-shadow-1 tc
+    dib f6 pa2 box-shadow-1 tc
     ${disabled ? 'moon-gray' : 'dark-gray'}
   `;
 
